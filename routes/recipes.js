23e511f45837ad3cd7ceab011914b6a9062b57ef
nofav1 <nofar67@gmail.com>
2025-05-25 18:59:09 +0300

    post recipes complete

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const recipes_utils = require("./utils/recipes_utils");
+const DButils = require("./utils/DButils");
 
 router.get("/", (req, res) => res.send("im here"));
 
@@ -17,6 +18,32 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
+/**
+ * This path allows an authenticated user to create a new recipe.
+ */
+router.post("/", async (req, res, next) => {
+  try {
+    // Ensure the user is authenticated via session
+    if (!req.session || !req.session.user_id) {
+      return res.status(401).send({ message: "Unauthorized: Please log in first" });
+    }
+    const user_id = req.session.user_id;
 
+    // Extract recipe fields from request body
+    const { id, title, image, duration, likes, vegan, vegetarian, glutenFree, viewed, ingredients, steps, servings } = req.body;
+    // (Optionally, validate that none of these fields are undefined/null here)
+
+    // Use the recipes_utils helper to insert the new recipe into the DB
+    await recipes_utils.insertRecipe(user_id, {
+      id, title, image, duration, likes, vegan, vegetarian, glutenFree, viewed, ingredients, steps, servings
+    });
+
+    // If successful, return a 201 response with a success message
+    res.status(201).send({ message: "Recipe created successfully", success: true });
+  } catch (error) {
+    // In case of any error, forward it to the error handler middleware
+    next(error);
+  }
+});
 
 module.exports = router;
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const api_domain = "https://api.spoonacular.com/recipes";
-
+const DButils = require("./DButils");
 
 
 /**
@@ -52,8 +52,43 @@ async function getRecipesPreview(recipe_ids) {
   }
 }
 
+/**
+ * Insert a custom recipe into the user_recipes table
+ */
+async function insertRecipe(user_id, recipeData) {
+  const {
+    id, //not in use
+    title,
+    image,
+    duration,
+    vegan,
+    vegetarian,
+    glutenFree,
+    ingredients,
+    steps,
+    servings,
+  } = recipeData;
+
+  const ingredientsJSON = JSON.stringify(ingredients);
+  const instructions = JSON.stringify(steps);
+
+  const veganVal = vegan ? 1 : 0;
+  const vegetarianVal = vegetarian ? 1 : 0;
+  const glutenFreeVal = glutenFree ? 1 : 0;
+
+  const insertQuery = `
+  INSERT INTO user_recipes 
+    (user_id, title, image, prep_time, servings, instructions, ingredients, is_vegan, is_vegetarian, is_gluten_free)
+  VALUES 
+    (${user_id}, '${title.replace(/'/g, "''")}', '${image.replace(/'/g, "''")}', ${duration}, ${servings}, '${instructions}', '${ingredientsJSON}', ${veganVal}, ${vegetarianVal}, ${glutenFreeVal});
+    `;
+
+  await DButils.execQuery(insertQuery);
+}
+
 exports.getRecipeDetails = getRecipeDetails;
 exports.getRecipesPreview = getRecipesPreview;
+exports.insertRecipe = insertRecipe;
 
 
 
