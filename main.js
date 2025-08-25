33fe91f77d058f4e98014ce80d0790c9b8c2064f
hallelhel <hallelh@post.bgu.ac.il>
2025-08-25 19:35:40 +0300

    domain configuration

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log("The content of ENV FROM MAIN: ", process.env.spoonacular_apiKey)
 //#region express configures
 var express = require("express");
 var path = require("path");
@@ -37,17 +38,16 @@ app.use(
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 //local:
-app.use(express.static(path.join(__dirname, "dist")));
+//app.use(express.static(path.join(__dirname, "dist")));
 //remote:
-// app.use(express.static(path.join(__dirname, '../assignment-3-3-frontend/dist')));
+app.use(express.static(path.join(__dirname, '../assignment3-3-211648126_313557969_assignment2/dist')));
 
 app.get("/",function(req,res)
 { 
   //remote: 
-  // res.sendFile(path.join(__dirname, '../assignment-3-3-frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../assignment3-3-211648126_313557969_assignment2/dist/index.html'));
   //local:
-  res.sendFile(__dirname+"/index.html");
-
+  //res.sendFile(__dirname+"/index.html");
 });
 
 app.use(cors());
@@ -106,13 +106,15 @@ app.use(function (err, req, res, next) {
 
 
 
-const server = app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
+// const server = app.listen(port, () => {
+//   console.log(`Server listen on port ${port}`);
+// });
 
-process.on("SIGINT", function () {
-  if (server) {
-    server.close(() => console.log("server closed"));
-  }
-  process.exit();
-});
+// process.on("SIGINT", function () {
+//   if (server) {
+//     server.close(() => console.log("server closed"));
+//   }
+//   process.exit();
+// });
+
+module.exports = app;
